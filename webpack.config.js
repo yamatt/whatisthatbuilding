94b2bb3eabba47446ec857af941ad8a5f32b3c1e
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2026-01-18 21:40:22 +0000

    Converting to webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  externals: {
+    aframe: 'AFRAME'
+  },
+  devServer: {
+    static: './',
+    hot: true,
+    server: 'https', // IMPORTANT: Required for Camera/GPS
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
