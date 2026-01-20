02102430ecaecde9e084dcd51e5c6208d0898a80
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2026-01-20 22:09:28 +0000

    Converting to canvas
    This doesn't work, but need to capture the change

--- src/index.js
+++ src/index.js
@@ -1,37 +1,135 @@
-// 1. Get user's current position
-navigator.geolocation.getCurrentPosition((position) => {
-    const { latitude, longitude } = position.coords;
-    console.log("Location", latitude, longitude)
-    fetchBuildings(latitude, longitude);
-});
+class Hud {
+    FOV_DEGREES = 60;
 
-async function fetchBuildings(lat, lon) {
-    // 2. Overpass API Query
-    // Finds buildings with height data within 5km (5000m)
-    const offset = 0.15;
-    const south = lat - offset;
-    const west = lon - offset;
-    const north = lat + offset;
-    const east = lon + offset;
+    constructor(canvas_id, buildings) {
+        this.canvas_id = canvas_id
+        this.buildings = buildings;
+    }
 
-    // 2. Query only for buildings with 3-digit heights (100m+)
-    // or 2-digit levels starting with 3 or more (30+ floors)
-    const query = `
-        [out:json][timeout:10];
-        (
-        way["building"]["height"~"^[1-9][0-9][0-9]"](${south},${west},${north},${east});
-        relation["building"]["height"~"^[1-9][0-9][0-9]"](${south},${west},${north},${east});
-        way["building"]["building:levels"~"^[3-9][0-9]"](${south},${west},${north},${east});
-        );
-        out center 15;
-    `;
-    const url = "https://overpass-api.de/api/interpreter?data=" + encodeURIComponent(query);
+    get canvas() {
+        return document.getElementById(this.canvas_id);
+    }
 
-    const response = await fetch(url);
-    const data = await response.json();
+    get context() {
+        return this.canvas.getContext('2d');
+    }
 
-    // 3. Filter for the "Tallest" (Sorting by OSM tags)
-    const buildings = data.elements
+    resize() {
+        const dpr = window.devicePixelRatio || 1;
+
+        this.canvas.width  = window.innerWidth * dpr;
+        thiscanvas.height = window.innerHeight * dpr;
+
+        this.canvas.style.width  = window.innerWidth + 'px';
+        this.canvas.style.height = window.innerHeight + 'px';
+
+        this.context.setTransform(dpr, 0, 0, dpr, 0, 0);
+    }
+
+    draw() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.buildings.forEach(building => this.drawMarker(building));
+        requestAnimationFrame(draw);
+    }
+
+    get screenCenterX () {
+        return window.innerWidth / 2;
+    }
+
+    get screenCenterY () {
+        return window.innerHeight / 2;
+    }
+
+    drawMarker(building) {
+        building.relativeBearing = ((building.bearing - smoothedHeading + 540) % 360) - 180;
+
+        const x = this.screenCenterX + building.screenOffsetX;
+        const y = this.screenCenterY - 40;
+
+        this.context.globalAlpha = building.alpha;
+
+        this.context.fillStyle = 'rgba(0,0,0,0.6)';
+        this.context.fillRect(x - 60, y - 24, 120, 32);
+
+        this.context.fillStyle = 'white';
+        this.context.font = '16px sans-serif';
+        this.context.textAlign = 'center';
+        this.context.fillText(b.name, x, y);
+    }
+
+    start() {
+        this.resize()
+        window.addEventListener('resize', resize);
+    }
+}
+
+class Camera {
+    CONSTRAINTS = {
+        video: { facingMode: 'environment' }
+    }
+
+    constructor(el_id) {
+        this.el_id = el_id
+    }
+
+    get el() {
+        return document.getElementById(this.el_id);
+    }
+
+    async start() {
+        const stream = await navigator.mediaDevices.getUserMedia(this.CONSTRAINTS);
+        this.el.srcObject = stream;
+        this.el.play();
+    }
+}
+
+class Buildings {
+    OFFSET = 0.15
+    API_URL = "https://overpass-api.de/api/interpreter?data="
+
+    constructor(latitude, longitude) {
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+
+    get north() {
+        return this.latitude + this.OFFSET;
+    }
+
+    get south() {
+        return this.latitude - this.OFFSET;
+    }
+
+    get east() {
+        return this.longitude + this.OFFSET;
+    }
+
+    get west() {
+        return this.longitude - this.OFFSET;
+    }
+
+    get query() {
+        return `
+            [out:json][timeout:10];
+            (
+            way["building"]["height"~"^[1-9][0-9][0-9]"](${this.south},${this.west},${this.north},${this.east});
+            relation["building"]["height"~"^[1-9][0-9][0-9]"](${this.south},${this.west},${this.north},${this.east});
+            way["building"]["building:levels"~"^[3-9][0-9]"](${this.south},${this.west},${this.north},${this.east});
+            );
+            out center 15;
+        `
+    }
+
+    get url() {
+        const encodedQuery = encodeURIComponent(this.query)
+        return `${this.API_URL}${encodedQuery}`
+    }
+
+    async getBuildings() {
+        const response = await fetch(this.url);
+        const data = await response.json();
+
+        const buildings = data.elements
         .map(b => ({
             name: b.tags.name || "Building",
             height: parseFloat(b.tags.height || b.tags["building:levels"] * 3 || 0),
@@ -39,38 +137,29 @@ async function fetchBuildings(lat, lon) {
             lon: b.center.lon
         }))
         .sort((a, b) => b.height - a.height)
-        .slice(0, 5); // Take top 5 tallest
-    console.log("Buildings", buildings.length, buildings)
-    renderMarkers(buildings);
+        .slice(0, 5);
+
+        return buildings;
+    }
 }
 
-function renderMarkers(buildings) {
-    const container = document.querySelector('#building-markers');
+window.addEventListener('DOMContentLoaded', () => {
+    const camera = new Camera("camera");
+    camera.start();
 
-    createMarker({
-        name: "TEST",
-        height: 100,
-        lat: latitude + 0.001,
-        lon: longitude
-    }, container);
+    const { latitude, longitude } = (null, null)
 
-    buildings.forEach(b => {
-        createMarker(b, container)
+    navigator.geolocation.getCurrentPosition((position) => {
+        latitude = position.coords[0];
+        longitude = position.coords[1];
     });
-}
 
-function createMarker(building, container) {
-    const entity = document.createElement('a-entity');
-    entity.setAttribute('gps-entity-place', `latitude: ${building.lat}; longitude: ${building.lon};`);
+    console.log("Location", latitude, longitude)
 
-    // Add a visual 3D label (a text box)
-    entity.setAttribute('geometry', 'primitive: box; height: 2; width: 2; depth: 0.1');
-    entity.setAttribute('material', 'color: #00FF00; shader: flat; opacity: 0.8');
-    entity.setAttribute('text', `value: ${building.name}\n${building.height}m; align: center; width: 50;`);
-    entity.setAttribute('position', '0 5 0'); // lift it 5m above ground
+    const buildingQuery = new Buildings(latitude, longitude);
+    const buildings = await buildingQuery.getBuildings();
 
-    // Ensure it faces the user
-    entity.setAttribute('look-at', '[gps-camera]');
+    const hud = new Hud("hud", buildings);
+    hud.start();
+});
 
-    container.appendChild(entity);
-}
